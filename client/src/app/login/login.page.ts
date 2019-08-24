d601544966e07bdf6a8329978881727b725934af
Ralph Schaer <ralphschaer@gmail.com>
2019-08-24 14:01:55 +0200

    Simplified version with @github/webauthn-json

--- client/src/app/login/login.page.ts
+++ client/src/app/login/login.page.ts
@@ -1,9 +1,9 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../auth.service';
 import {LoadingController, NavController} from '@ionic/angular';
-import {base64ToUint8Array, uint8ArrayTobase64} from '../util';
 import {MessagesService} from '../messages.service';
 import {HttpClient} from '@angular/common/http';
+import {get} from '@github/webauthn-json';
 
 @Component({
   selector: 'app-login',
@@ -31,35 +31,21 @@ export class LoginPage {
   }
 
   private async handleAssertionStart(response) {
-    const getOptions = response.publicKeyCredentialRequestOptions;
-    getOptions.challenge = base64ToUint8Array(getOptions.challenge);
-    getOptions.allowCredentials.forEach(listItem => listItem.id = base64ToUint8Array(listItem.id));
-
-    // @ts-ignore
-    const assertion = await navigator.credentials.get({
-      publicKey: getOptions
+    const credential = await get({
+      publicKey: response.publicKeyCredentialRequestOptions
     });
 
-    let clientExtensionResults = {};
     try {
-      clientExtensionResults = assertion.getClientExtensionResults();
+      // @ts-ignore
+      credential.clientExtensionResults = credential.getClientExtensionResults();
     } catch (e) {
-      console.error('getClientExtensionResults failed', e);
+      // @ts-ignore
+      credential.clientExtensionResults = {};
     }
 
     const assertionResponse = {
       assertionId: response.assertionId,
-      credential: {
-        id: assertion.id,
-        type: assertion.type,
-        clientExtensionResults,
-        response: {
-          authenticatorData: uint8ArrayTobase64(assertion.response.authenticatorData),
-          clientDataJSON: uint8ArrayTobase64(assertion.response.clientDataJSON),
-          signature: uint8ArrayTobase64(assertion.response.signature),
-          userHandle: assertion.response.userHandle !== null ? uint8ArrayTobase64(assertion.response.userHandle) : null,
-        }
-      }
+      credential
     };
 
     const loading = await this.messagesService.showLoading('Validating ...');
