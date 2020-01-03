e258f85578d61bde729b397693a13cf3555f268a
Ralph Schaer <ralphschaer@gmail.com>
2020-01-03 06:23:51 +0100

    Upgrade libraries

--- server/src/main/java/ch/rasc/webauthn/security/SecurityConfig.java
+++ server/src/main/java/ch/rasc/webauthn/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().logout(customizer -> {
+    http.csrf(customizer -> customizer.disable()).logout(customizer -> {
       customizer.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
       customizer.deleteCookies("JSESSIONID");
     }).authorizeRequests(customizer -> {
