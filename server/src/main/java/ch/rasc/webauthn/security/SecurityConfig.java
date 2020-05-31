c857605d1cfdb534429030aa9572ddd9cd4d078a
Ralph Schaer <ralphschaer@gmail.com>
2020-05-31 06:16:26 +0200

    Upgrade libraries

--- server/src/main/java/ch/rasc/webauthn/security/SecurityConfig.java
+++ server/src/main/java/ch/rasc/webauthn/security/SecurityConfig.java
@@ -1,9 +1,5 @@
 package ch.rasc.webauthn.security;
 
-import java.util.Map;
-
-import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -14,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
-import org.springframework.web.context.request.WebRequest;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -42,17 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       customizer.anyRequest().authenticated();
     }).exceptionHandling(customizer -> customizer
         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
-  }
-
-  @Bean
-  public ErrorAttributes errorAttributes() {
-    return new DefaultErrorAttributes() {
-      @Override
-      public Map<String, Object> getErrorAttributes(WebRequest webRequest,
-          boolean includeStackTrace) {
-        return Map.of();
-      }
-    };
   }
 
 }
